0f909bec49acd92cefd8dea6e98f2a69b84f089b
James Attfield <jamesattfield98@gmail.com>
2024-08-04 15:32:11 +0100

    added success msgs to ClaimSubCommand

--- PixelMCDonations/PixelMCDonations/src/main/java/co/pixelmc/pixelmcdonations/commands/claim/ClaimSubCommand.java
+++ PixelMCDonations/PixelMCDonations/src/main/java/co/pixelmc/pixelmcdonations/commands/claim/ClaimSubCommand.java
@@ -2,16 +2,38 @@ package co.pixelmc.pixelmcdonations.commands.claim;
 
 import co.pixelmc.pixelmcdonations.PixelMCDonations;
 import co.pixelmc.pixelmcdonations.commands.AbstractSubCommand;
+import co.pixelmc.pixelmcdonations.logic.RanksLogic;
 import co.pixelmc.pixelmcdonations.models.Player;
+import co.pixelmc.pixelmcdonations.models.Rank;
 import co.pixelmc.pixelmcdonations.repositories.DonationRepository;
 import co.pixelmc.pixelmcdonations.repositories.PlayerRepository;
+import co.pixelmc.pixelmcdonations.stringops.MinecraftColour;
+import co.pixelmc.pixelmcdonations.stringops.MinecraftTextBuilder;
+import co.pixelmc.pixelmcdonations.stringops.MinecraftTextBuilderLine;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+
+import java.util.Arrays;
 
 public class ClaimSubCommand extends AbstractSubCommand {
     @Override
     protected String getLabel() {
         return "claim";
+    }
+
+    private void sendSuccessMsg(CommandSender sender, Rank[] ranks){
+        MinecraftTextBuilder builder = new MinecraftTextBuilder()
+                .addLine(MinecraftTextBuilderLine.of(MinecraftColour.GOLD, "You have claimed the following ranks: "));
+
+        Arrays.stream(ranks)
+                .forEach(x ->
+                        builder.addLine(
+                                MinecraftTextBuilderLine.of(MinecraftColour.AQUA, " - " + x.getRankName())
+                        )
+                );
+
+        String[] msg = builder.getLines();
+        sender.sendMessage(msg);
     }
 
     @Override
@@ -26,7 +48,12 @@ public class ClaimSubCommand extends AbstractSubCommand {
         if (player == null)
             return false;
 
-        donationRepository.claimAvailableRanks(player);
+        Rank[] ranks = donationRepository.claimAvailableRanks(player).toArray(Rank[]::new);
+
+        RanksLogic ranksLogic = new RanksLogic();
+        ranksLogic.claimRanks(player, ranks);
+
+        sendSuccessMsg(sender, ranks);
 
         return true;
     }
