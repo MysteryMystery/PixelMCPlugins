6070100ef3d85848e3a0a9f9547e188d74e0ddc5
James Attfield <jamesattfield98@gmail.com>
2024-08-04 15:24:54 +0100

    added placeholders to claim RanksLogic.java

--- PixelMCDonations/PixelMCDonations/src/main/java/co/pixelmc/pixelmcdonations/logic/RanksLogic.java
+++ PixelMCDonations/PixelMCDonations/src/main/java/co/pixelmc/pixelmcdonations/logic/RanksLogic.java
@@ -2,13 +2,17 @@ package co.pixelmc.pixelmcdonations.logic;
 
 import co.pixelmc.pixelmcdonations.models.Player;
 import co.pixelmc.pixelmcdonations.models.Rank;
+import co.pixelmc.pixelmcdonations.stringops.PlaceHolderStringHandler;
 import org.bukkit.Bukkit;
 
 public class RanksLogic {
     public boolean claimRanks(Player player, Rank ... ranks){
+        PlaceHolderStringHandler placeholders = new PlaceHolderStringHandler()
+                .replaceUUID(player.getPlayerUuid());
+
         for (Rank rank : ranks) {
-            //TODO placeholders
-            Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), rank.getCommand());
+            String command = placeholders.getString(rank.getCommand());
+            Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), command);
         }
 
         return true;
